8b1770046ad6ddedb45c065a52f0a52150ac9d35
Anésio Neto <anesios98@gmail.com>
2024-04-22 18:39:45 -0300

    :hammer: Adicionada configuração da palheta de cores ao tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-100": "#D9D9D9",
+        "gray-300": "#7C7A7A",
+        "primary-100": "#323237",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#2F2F39",
+        "secondary-400": "#F4CA01",
+        "secondary-500": "#FFC132",
+      },
+    },
   },
   plugins: [],
 };
