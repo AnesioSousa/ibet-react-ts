413cec3e9ebb2c0dcbf3fe5389a3f1255e00f55b
Anésio Neto <anesios98@gmail.com>
2024-04-23 19:30:03 -0300

    :card_file_box: Adicionados dados de mockup

---
+++ src/data/matches-data.js
@@ -0,0 +1,8 @@
+// Partidas
+[
+    { timeDaCasa: "Flamengo", timeVisitante: "Cruzeiro", campeonato: "Campeonato Brasileiro" },
+    { timeDaCasa: "São Paulo", timeVisitante: "Palmeiras", campeonato: "Campeonato Brasileiro" },
+    { timeDaCasa: "Grêmio", timeVisitante: "Internacional", campeonato: "Campeonato Brasileiro" },
+    { timeDaCasa: "Atlético Mineiro", timeVisitante: "Corinthians", campeonato: "Campeonato Brasileiro" },
+    { timeDaCasa: "Fluminense", timeVisitante: "Botafogo", campeonato: "Campeonato Brasileiro" }
+  ]
