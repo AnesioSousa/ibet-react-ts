64989e7680e8e7d13246cb8bf4f0ff5cf3c2bbec
Anésio Neto <anesios98@gmail.com>
2024-04-22 21:39:44 -0300

    :hammer: Remoção de imports não utilizados

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,11 +1,9 @@
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
-import { M } from "vite/dist/node/types.d-aGj9QkWt";
-import { afterEach, describe, expect, it } from "vitest";
+import { afterEach, expect } from "vitest";
 
 expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
 });
-
