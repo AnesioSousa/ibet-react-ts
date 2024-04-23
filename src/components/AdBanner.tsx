b423e4cf8faae52db823daa79a93e742412e70f1
Anésio Neto <anesios98@gmail.com>
2024-04-23 17:23:02 -0300

    :star: Implementação do banner de propaganda

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/16/solid";
 
 const AdBanner = () => {
-  return <div>AdBanner</div>;
+  return (
+    <div className="py-12 text-white flex items-center justify-between bg-primary-300 text-3xl">
+      <button className="px-1">
+        <ChevronLeftIcon className="h-14 w-14 text-secondary-500" />
+      </button>
+      <p>
+        Anúncios/chamadas que podem ser <br /> trocados pelos funcionários
+      </p>
+      <button className="px-1">
+        <ChevronRightIcon className="h-14 w-14 text-secondary-500" />
+      </button>
+    </div>
+  );
 };
 
 export default AdBanner;
