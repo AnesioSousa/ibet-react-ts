c7c2ee147430066e4256fd0e41e63a80f286e3cf
Anésio Neto <anesios98@gmail.com>
2024-04-24 14:01:46 -0300

    :card_file_box: Atualização nos dados de partidas

--- src/data/matches-data.js
+++
@@ -1,8 +0,0 @@
-// Partidas
-[
-    { timeDaCasa: "Flamengo", timeVisitante: "Cruzeiro", campeonato: "Campeonato Brasileiro" },
-    { timeDaCasa: "São Paulo", timeVisitante: "Palmeiras", campeonato: "Campeonato Brasileiro" },
-    { timeDaCasa: "Grêmio", timeVisitante: "Internacional", campeonato: "Campeonato Brasileiro" },
-    { timeDaCasa: "Atlético Mineiro", timeVisitante: "Corinthians", campeonato: "Campeonato Brasileiro" },
-    { timeDaCasa: "Fluminense", timeVisitante: "Botafogo", campeonato: "Campeonato Brasileiro" }
-  ]
---
+++ src/data/matchs-data.js
@@ -0,0 +1,36 @@
+// Partidas
+
+const matchs = [
+  {
+    id: 0,
+    timeDaCasa: "Flamengo",
+    timeVisitante: "Cruzeiro",
+    campeonato: "Brasileiro",
+  },
+  {
+    id: 1,
+    timeDaCasa: "São Paulo",
+    timeVisitante: "Palmeiras",
+    campeonato: "Brasileiro",
+  },
+  {
+    id: 2,
+    timeDaCasa: "Grêmio",
+    timeVisitante: "Internacional",
+    campeonato: "Brasileiro",
+  },
+  {
+    id: 3,
+    timeDaCasa: "Atlético Mineiro",
+    timeVisitante: "Corinthians",
+    campeonato: "Brasileiro",
+  },
+  {
+    id: 4,
+    timeDaCasa: "Fluminense",
+    timeVisitante: "Botafogo",
+    campeonato: "Brasileiro",
+  },
+];
+
+export default matchs;
