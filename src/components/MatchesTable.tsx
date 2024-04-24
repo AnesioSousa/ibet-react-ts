d72f4f55fa369a178de4a6ff5d4edb1d1d8b34f2
Anésio Neto <anesios98@gmail.com>
2024-04-24 14:02:39 -0300

    :construction: implementação da tabela de partidas

--- src/components/MatchesTable.tsx
+++ src/components/MatchesTable.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
+import matchs from "../data/matchs-data.js";
 
-const MatchesTable = () => {
-  return <div>MatchesTable</div>;
+type Partida = {
+  id: number;
+  timeDaCasa: string;
+  timeVisitante: string;
+  campeonato: string;
 };
+
+function MatchesTable() {
+  const matchList = matchs.map((partida: Partida) => (
+    <li key={partida.id}>
+      <button className="w-full border-2 border-black flex items-center justify-between">
+        <button className="rounded-xl px-5 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+          {partida.campeonato}
+        </button>
+        <div className="flex items-center ">
+          <button className="px-8 rounded-2xl bg-secondary-500 text-white">
+            {partida.timeDaCasa}
+          </button>
+          <span className="place-self-center text-white h-8 w-14 rounded-2xl inline-block mx-8 bg-primary-700">
+            VS
+          </span>
+          <button className="px-8 rounded-2xl bg-secondary-500 text-white">
+            {partida.timeVisitante}
+          </button>
+        </div>
+        <ChevronDownIcon className="h-7 w-7 text-black" />
+      </button>
+    </li>
+  ));
+  return <ul>{matchList}</ul>;
+}
 
 export default MatchesTable;
